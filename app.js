aa76fb2e5a6ad6e236c10e3ef0f20acc43e2e92c
fabyup <afabyblasco@gmail.com>
2024-12-02 23:55:08 -0300

    agregue hola mundo

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ app.use(json());
 app.use(cors());
 
 app.use("/api", userRoutes);
+app.get("/saludo", (req, res) => {
+  res.send("Hola, mundo!");
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,5 +4,8 @@ import userController from "../controllers/userController.js";
 const router = Router();
 router.get("/usuarios", userController.obtenerUsuarios);
 router.post("/usuarios", userController.crearUsuario);
+router.get("/saludo", (req, res) => {
+  res.send("hola mundo");
+});
 
 export default router;
